6ac7984f293df9852c983b0807df1fff42899383
Sushant Pandey <sushaantkp@outlook.in>
2023-10-03 09:26:41 +0530

    Logo font changed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,15 @@
 import styles from "./Header.module.css";
 
-import { Yeon_Sung } from "next/font/google";
+import { Black_Ops_One } from "next/font/google";
 
-const yeonSung = Yeon_Sung({subsets: ["latin"], weight: "400"})
+const blackOpsOne = Black_Ops_One({subsets: ["latin"], weight: "400"})
 
 export default function Header() {
 
     return (
         <header className={`${styles.header} mb-4`} >
             <div style={{ display: "flex" }}>
-                <h1 className={`${styles.heading} font-extrabold ${yeonSung.className}`}>
+                <h1 className={`${styles.heading} font-extrabold ${blackOpsOne.className}`}>
                     WeatherUp
                 </h1>
             </div>
